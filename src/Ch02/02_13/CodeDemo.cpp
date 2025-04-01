5b2f3a807d143aabe1a0b83de6f764e66bcee0cf
Aaron Wright <133648359+MilkTeaDuck@users.noreply.github.com>
2025-04-01 06:17:21 +0000

    CH2 Complete

--- src/Ch02/02_13/CodeDemo.cpp
+++ src/Ch02/02_13/CodeDemo.cpp
@@ -3,12 +3,14 @@
 // Calculate an Average, by Eduardo Corpeño 
 
 #include <iostream>
+#define TOTAL 5
 
 int main(){
-    int nums[5] = {1,23,32,24,337};
+    float nums[TOTAL] = {1,23,32,24,337};
     float result; 
 
     // Write your code here
+    result = (nums[0] + nums[1] + nums[2] + nums[3] + nums[4])/TOTAL;
     
     std::cout << "The average is " << result << std::endl;
     
