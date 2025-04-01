443f4d4389f5b47597546229f88cabec5dd636f6
Aaron Wright <133648359+MilkTeaDuck@users.noreply.github.com>
2025-04-01 08:35:09 +0000

    CH3 P1->9

--- src/Ch03/03_09b/CodeDemo.cpp
+++ src/Ch03/03_09b/CodeDemo.cpp
@@ -2,11 +2,23 @@
 // Exercise 03_09
 // Vectors, by Eduardo Corpeño 
 
+#include <vector>
 #include <iostream>
 #include <string>
 
 int main(){
+    std::vector<int> primes;
     
+    primes.push_back(2);
+    primes.push_back(5);
+    primes.push_back(4);
+    primes.push_back(7);
+    primes.push_back(3);
+
+    
+
+    std::cout << "This vector has " << primes.size() << " elements." << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
